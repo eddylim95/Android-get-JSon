228745f4d5f3943ae684b87acd6014b094d3131a
Eddy Lim Qing Yang <eddylim95@hotmail.com>
2018-03-28 20:55:14 +0800

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -7,9 +7,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ProgressBar;
-
 
 import com.android.volley.Cache;
 import com.android.volley.Network;
@@ -24,17 +21,19 @@ import com.android.volley.toolbox.JsonObjectRequest;
 
 import org.json.JSONObject;
 
-import java.net.InetAddress;
-
 public class MainActivity extends AppCompatActivity {
 
     private String url = "https://developers.onemap.sg/privateapi/";
     private String urlType = "themesvc/retrieveTheme?queryName=wireless_hotspots&token=";
-    private String apiKey = "";
+    private String apiKey = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEzOTEsInVzZXJf" +
+            "aWQiOjEzOTEsImVtYWlsIjoiZWRkeWxpbTk1QGhvdG1haWwuY29tIiwiZm9yZXZlciI6ZmFsc2UsImlzc" +
+            "yI6Imh0dHA6XC9cL29tMi5kZmUub25lbWFwLnNnXC9hcGlcL3YyXC91c2VyXC9zZXNzaW9uIiwiaWF0I" +
+            "joxNTIyMDY3ODYyLCJleHAiOjE1MjI0OTk4NjIsIm5iZiI6MTUyMjA2Nzg2MiwianRpIjoiZmU0YTBkOGVjN" +
+            "WY2ZjEzMGJhOGFkZGNmOTQ2NDAzNzkifQ.zlkHGk-zlqzsSMsHKDLBxo1uS1lcd5XkEbuisv6g2As";
 
-    private int progress = 0;
     JSONObject json;
     AppDatabase mDB;
+    Boolean internetConnection;
 
 
     @Override
@@ -45,32 +44,25 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        RequestQueue mRequestQueue;
-        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
-        final Network network = new BasicNetwork(new HurlStack());
-        mRequestQueue = new RequestQueue(cache, network);
-        mRequestQueue.start();
+        try {
+            RequestQueue mRequestQueue;
+            Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024);
+            final Network network = new BasicNetwork(new HurlStack());
+            mRequestQueue = new RequestQueue(cache, network);
+            mRequestQueue.start();
+        }
+        catch (Exception e){
+            Log.e("MainActivity", "Request Queue error", e);
+        }
 
-        EditText editText = (EditText) findViewById(R.id.editSearch);
-        ProgressBar progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        progress = 0;
-        progressBar.setProgress(progress);
-
-        Button button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                isInternetAvailable();
-            }
-        });
+        InternetConnection internetConnection = InternetConnection.getInternetStatus();
+        internetConnection.getInternet();
 
         mDB = AppDatabase.getInstance(getApplicationContext());
-        isInternetAvailable();
         retrieveJson();
 
         try {
-            final Button button = (Button) findViewById(R.id.button);
+            Button button = (Button) findViewById(R.id.button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -85,26 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-    public boolean isInternetAvailable() {
-        try {
-            InetAddress ipAddr = InetAddress.getByName("google.com");
-            //You can replace it with your name
-            if (!ipAddr.equals("") == true){
-                Log.d("Internet","Internet connected");
-            }
-            else {
-                Log.d("Internet","Internet not connected");
-            }
-            return !ipAddr.equals("");
-
-        } catch (Exception e) {
-            Log.e("Internet Error", "exception", e);
-            return false;
-        }
-    }
-
     private void retrieveJson() {
         try {
             String urls = this.getConcatUrl();
@@ -116,18 +88,18 @@ public class MainActivity extends AppCompatActivity {
                     try {
                         json = response;
                     } catch (Exception e) {
-                        Log.e("MYAPP", "exception", e);
+                        Log.e("Json", "Failed assign Json", e);
                     }
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
-                    Log.e("json return error", "exception", error);
+                    Log.e("Json", "Volley error", error);
                 }
             });
             MySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjectRequest);
         } catch (Exception e) {
-            Log.e("Request", "exception", e);
+            Log.e("Json", "JsonObjectRequest failed", e);
         }
     }
 
